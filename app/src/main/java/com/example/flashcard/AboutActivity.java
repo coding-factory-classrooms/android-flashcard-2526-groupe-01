3a5ad2557bb55e274c6c1dc1671e52a3f60535b7
DraxanLT <draxan.launay-tran@edu.esiee-it.fr>
2025-10-07 23:40:07 +0200

    Add-Edit | UI for AA.xml & extras MA to AA

--- app/src/main/java/com/example/flashcard/AboutActivity.java
+++ app/src/main/java/com/example/flashcard/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.example.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +25,29 @@ public class AboutActivity extends AppCompatActivity {
             return insets;
         });
 
+        Intent srcIntent = getIntent();
+        int appTitle = srcIntent.getIntExtra("apptitle", R.drawable.apptitle);
+        String appTitleText = srcIntent.getStringExtra("apptitletext");
+        String creatorI = srcIntent.getStringExtra("creatorI");
+        String creatorII = srcIntent.getStringExtra("creatorII");
+        String creatorIII = srcIntent.getStringExtra("creatorIII");
+        String creatorIV = srcIntent.getStringExtra("creatorIV");
+        String group = srcIntent.getStringExtra("group");
+        String version = srcIntent.getStringExtra("version");
 
+        ImageView titleImageView = findViewById(R.id.titleImageView);
+        TextView titleTextView = findViewById(R.id.titleTextView);
+        TextView creatorsTextView = findViewById(R.id.creatorsTextView);
+        TextView groupTextView = findViewById(R.id.groupTextView);
+        TextView versionTextView = findViewById(R.id.versionTextView);
+        titleImageView.setImageResource(appTitle);
+        titleTextView.setText(appTitleText);
+        creatorsTextView.setText("Creators : " +
+                creatorI + ", " +
+                creatorII + ", " +
+                creatorIII + ", " +
+                creatorIV);
+        groupTextView.setText("Group : " + group);
+        versionTextView.setText("Version : " + version);
     }
 }
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = "MainActivity";
 
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private Button questionsListButton;
     private Button statButton;
     private Button aboutButton;
+
+    // All variables for Extras
+    private int appTitle = R.drawable.apptitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +38,47 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ImageView titleImageView = findViewById(R.id.titleImageView);
+        titleImageView.setImageResource(appTitle);
+
         // Here is where we're linking our variables "Button" to their equivalents in activity_main.xml
         startButton = findViewById(R.id.startButton);
         questionsListButton = findViewById(R.id.questionsListButton);
         statButton = findViewById(R.id.statButton);
         aboutButton = findViewById(R.id.aboutButton);
 
-        // Here is where we're going to put all extras
+        // Heart of the activity
+        startButton.setOnClickListener(this);
+        questionsListButton.setOnClickListener(this);
+        statButton.setOnClickListener(this);
+        aboutButton.setOnClickListener(this);
+    }
 
-
-
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.startButton) {
+            // choose a difficulty
+        } else if (id == R.id.questionsListButton) {
+            // go to questionsListActivity
+            Intent intent = new Intent(this, QuestionsListActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.statButton) {
+            // go to statActivity
+            Intent intent = new Intent(this, StatActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.aboutButton) {
+            // go to aboutActivity
+            Intent intent = new Intent(this, AboutActivity.class);
+            intent.putExtra("apptitle", appTitle);
+            intent.putExtra("apptitletext", "Guess the flag");
+            intent.putExtra("creatorI", "Draxan LT");
+            intent.putExtra("creatorII", "Matias D");
+            intent.putExtra("creatorIII", "Strauss A");
+            intent.putExtra("creatorIV", "Romain P");
+            intent.putExtra("group", "01");
+            intent.putExtra("version", "prototype 1");
+            startActivity(intent);
+        }
     }
 }
